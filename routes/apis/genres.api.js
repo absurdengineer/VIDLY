5a56a29e21b4339618a51717182c1a8b6bc5bccf
mddalam1 <mddalam1@gmail.com>
2020-12-10 11:49:28 +0530

    Modularized code for standardization.

--- routes/apis/genres.api.js
+++ routes/apis/genres.api.js
@@ -1,24 +1,9 @@
 const express = require('express')
-const Joi = require('joi')
 const pool = require('../../databases/db')
+const {checkGenre, validateGenre} = require('../../models/genre.model')
 
 const router = express.Router()
 
-const genreSchema = Joi.object({
-    name : Joi.string().min(3).max(30).required()
-})
-const validateGenre = genre => {
-    return genreSchema.validate(genre)
-}
-const checkGenre = async id => {
-    try{
-        const result = await pool.query(`SELECT * FROM genres WHERE id=${id}`)
-        if(result.rowCount===0) return false
-        return result.rows[0]
-    } catch({name, message}){
-        console.error(`${name} : ${message}`)
-    }
-}
 router.get("/", async (req,res) => {
     try{
         const result = await pool.query('SELECT *  FROM genres')
